ba02f6d4df533df9b129dfd8a0d0f5f554cea3cf
Esenbek <75290400+EsenbekM@users.noreply.github.com>
2021-08-26 18:22:42 +0600

    Update hw_8_1.py

--- hw_8_1.py
+++ hw_8_1.py
@@ -1,1 +1,4 @@
-a, b, c = int(input()), int(input()), int(input())s = [a, b, c]s.sort()print(s[-1] - s[0])
+a, b, c = int(input()), int(input()), int(input())
+s = [a, b, c]
+s.sort()
+print(s[-1] - s[0]) 
